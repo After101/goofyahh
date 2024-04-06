d22cf026d8bd8ba37458c7463fc5996e3b7a012f
After101 <90823451+After101@users.noreply.github.com>
2024-04-06 12:08:50 +0530

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,17 +1,37 @@
+
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <AsyncWebSocket.h>
-#include "secure_storage.h"  // Include the library for secure storage
+
+const char *ssid = "";
+const char *password = "";
 
 AsyncWebServer server(80);
-AsyncWebSocket ws("ws://192.168.4.1/ws");
+AsyncWebSocket ws("/ws");
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  // Handle WebSocket messages
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->opcode == WS_TEXT) {
+    String message = "";
+    for (size_t i = 0; i < len; i++) {
+      message += (char)data[i];
+    }
+    
+    if (message == "LED_ON") {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("Button clicked: Turn LED On");
+    } else if (message == "LED_OFF") {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println("Button clicked: Turn LED Off");
+    }
+  }
 }
 
 void setup() {
-  WiFi.begin(getSSID(), getPassword());  // Use secure storage for SSID and password
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -20,6 +40,11 @@ void setup() {
 
   ws.onEvent(handleWebSocketMessage);
   server.addHandler(&ws);
+
+  // Enable CORS (Cross-Origin Resource Sharing)
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   server.begin();
 }
 
